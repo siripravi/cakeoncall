76d550fcdec95ecfd0e23d6145de685d6627ee16
provdigi <purnachandra.valluri@gmail.com>
2024-01-31 13:04:58 +0530

    jggtt

---
+++ themes/cakeBaker/views/layouts/auth.php
@@ -0,0 +1,13 @@
+<?php $this->beginContent('@app/themes/cakeBaker/views/layouts/home.php'); ?>
+
+<div class="container py-5">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-content" role="main">
+
+                <?php echo $content; ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->endContent(); ?>
