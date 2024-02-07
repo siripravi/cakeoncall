3184d8bb08ba39414ae5fe0a5b9002bdf997e825
provdigi <purnachandra.valluri@gmail.com>
2024-02-07 18:15:41 +0530

    categ

--- configs/config.php
+++ configs/config.php
@@ -22,7 +22,7 @@ $config = new Config('myproject', dirname(__DIR__), [
             //'viewMap' => ['block/*' =>'@app/views/blocks/']
 
         ],
-
+        'categoryadmin' => 'siripravi\category\admin\Module',
         'userauthfrontend' => [
             'class' => 'app\modules\userauth\frontend\Module',
             'useAppViewPath' => false, // When enabled the views will be looked up in the @app/views folder, otherwise the views shipped with the module will be used.
